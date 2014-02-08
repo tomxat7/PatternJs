9a3492e9ee23ce41b934362d606c12f2e7fda9a6
tomxat7 <tomxat7@gmail.com>
2014-02-08 12:42:47 +0900

    line

--- Pattern.js
+++ Pattern.js
@@ -4,82 +4,82 @@
 	* licensed under the MIT licenses. 
 */
 var PatternJs = {
-		status : 0,
+	status : 0,
 		
-		Core: function (_C,_Type,_Type2){ 
+	Core: function (_C,_Type,_Type2){ 
 		
-			if(!this.status) return 0;
+	if(!this.status) return 0;
 			
-					var i, _N1 = 0, _N2 = 0;
+		var i, _N1 = 0, _N2 = 0;
 					
-					switch(this.status){
+		switch(this.status){
 					
-						case "Pattern1" :	
+			case "Pattern1" :	
 						
-							for(i = 0; i < _C.length; ){
+				for(i = 0; i < _C.length; ){
 							
-								for(var q = 0; (parseInt(_C.charCodeAt( i++ ), 10) > 47 && parseInt(_C.charCodeAt( i-1 ), 10) < 58) || 
+					for(var q = 0; (parseInt(_C.charCodeAt( i++ ), 10) > 47 && parseInt(_C.charCodeAt( i-1 ), 10) < 58) || 
 								
-								(parseInt(_C.charCodeAt( i-1 ), 10) >64 && parseInt(_C.charCodeAt( i-1 ), 10) < 91) ||
+					(parseInt(_C.charCodeAt( i-1 ), 10) >64 && parseInt(_C.charCodeAt( i-1 ), 10) < 91) ||
 								
-								(parseInt(_C.charCodeAt( i-1), 10) >96 && parseInt(_C.charCodeAt( i-1 ), 10) < 123) ; q++);
+					(parseInt(_C.charCodeAt( i-1), 10) >96 && parseInt(_C.charCodeAt( i-1 ), 10) < 123) ; q++);
 								
-								if(q > 0) _N1++;
+						if(q > 0) _N1++;
 								
-								if(parseInt(_C.charCodeAt(i-1),10) === parseInt(_Type.charCodeAt(0), 10)) _N2++;
-							}
+						if(parseInt(_C.charCodeAt(i-1),10) === parseInt(_Type.charCodeAt(0), 10)) _N2++;
+				}
 							
-							if( _N1>1 && _N1-1 === _N2 ) return true;
+				if( _N1>1 && _N1-1 === _N2 ) return true;
 							
-							else  return false;
+				else  return false;
 							
-						case "Pattern2" : 
+			case "Pattern2" : 
 						
-							var first = false;
+				var first = false;
 							
-							var second = false;
+				var second = false;
 							
-							for(i = 0; i < _C.length; ){
+				for(i = 0; i < _C.length; ){
 							
-								for(var q = 0; (parseInt(_C.charCodeAt( i++ ), 10) > 47 && parseInt(_C.charCodeAt( i-1 ), 10) < 58) || 
+					for(var q = 0; (parseInt(_C.charCodeAt( i++ ), 10) > 47 && parseInt(_C.charCodeAt( i-1 ), 10) < 58) || 
 								
-								(parseInt(_C.charCodeAt( i-1 ), 10) >64 && parseInt(_C.charCodeAt( i-1 ), 10) < 91) || 
+					(parseInt(_C.charCodeAt( i-1 ), 10) >64 && parseInt(_C.charCodeAt( i-1 ), 10) < 91) || 
 								
-								(parseInt(_C.charCodeAt( i-1), 10) >96 && parseInt(_C.charCodeAt( i-1 ), 10) < 123) ; q++);
+					(parseInt(_C.charCodeAt( i-1), 10) >96 && parseInt(_C.charCodeAt( i-1 ), 10) < 123) ; q++);
 								
-								if(q > 0){
+						if(q > 0){
 								
-									if(!first) first = i-1;
+							if(!first) first = i-1;
 									
-									else if(!second) second = i-1;
+							else if(!second) second = i-1;
 									
-									_N1++;
-								}
+							_N1++;
+						}
 								
-								if(((parseInt(_C.charCodeAt( i-1 ),10) === parseInt(_Type.charCodeAt(0), 10) ||
+						if(((parseInt(_C.charCodeAt( i-1 ),10) === parseInt(_Type.charCodeAt(0), 10) ||
 								
-								parseInt(_C.charCodeAt( i-1 ),10) === parseInt(_Type2.charCodeAt(0), 10)) && !(first && second)) || 
+						parseInt(_C.charCodeAt( i-1 ),10) === parseInt(_Type2.charCodeAt(0), 10)) && !(first && second)) || 
 								
-								((first === i-1 && parseInt(_C.charCodeAt(first), 10) === parseInt(_Type.charCodeAt(0), 10)) ||
+						((first === i-1 && parseInt(_C.charCodeAt(first), 10) === parseInt(_Type.charCodeAt(0), 10)) ||
 								
-								(second === i-1 && parseInt(_C.charCodeAt(second),10) === parseInt(_Type2.charCodeAt(0), 10)))) _N2++;
+						(second === i-1 && parseInt(_C.charCodeAt(second),10) === parseInt(_Type2.charCodeAt(0), 10)))) _N2++;
 								
-							}
+				}
+								
+				if( _N1 === 3 && _N1-1 === _N2) return true;
 							
-							if( _N1 === 3 && _N1-1 === _N2) return true;
+				else  return false;
 							
-							else  return false;
-							
-						 default : //Bad selection 
+			default : //Bad selection 
 						 
-							return 0;
-					}
+				return 0;
+		}
 					
-				},	
+	},	
 				
-		Pattern1: function (_C,_Type){
+	Pattern1: function (_C,_Type){
 		
-			if(!_C || !_Type) return false;
+		if(!_C || !_Type) return false;
 			
 			this.status = "Pattern1";
 			
@@ -87,11 +87,11 @@ var PatternJs = {
 			
 			else return false;
 			
-		},
+	},
 		
-		Pattern2: function (_C,_Type,_Type2){
+	Pattern2: function (_C,_Type,_Type2){
 		
-			if(!_C || !_Type  || !_Type2) return false;
+		if(!_C || !_Type  || !_Type2) return false;
 			
 			this.status = "Pattern2";
 			
@@ -99,6 +99,6 @@ var PatternJs = {
 			
 			else return false;
 			
-		}
+	}
 		
 };
